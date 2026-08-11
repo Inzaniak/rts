30e4fae1d34b00c6351aff5b223f5598b39e2c42
Inzaniak <inzaniak@gmail.com>
2026-08-11 21:40:24 +0200

    Report module version for go installs

--- internal/buildinfo/buildinfo.go
+++ internal/buildinfo/buildinfo.go
@@ -1,8 +1,20 @@
 package buildinfo
 
+import "runtime/debug"
+
 // These values are replaced by the release workflow through -ldflags.
 var (
 	Version = "dev"
 	Commit  = "unknown"
 	Date    = "unknown"
 )
+
+func init() {
+	if Version != "dev" {
+		return
+	}
+	info, ok := debug.ReadBuildInfo()
+	if ok && info.Main.Version != "" && info.Main.Version != "(devel)" {
+		Version = info.Main.Version
+	}
+}
